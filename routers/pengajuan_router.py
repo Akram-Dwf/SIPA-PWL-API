d1948dd4adbdb5fd3c690c486901cb616d273f63
Akram-Dwf <akramtamir12@gmail.com>
2026-04-17 16:24:43 +0800

    menambah fitur get pengajuan by id

--- routers/pengajuan_router.py
+++ routers/pengajuan_router.py
@@ -33,6 +33,15 @@ def create_pengajuan(pengajuan: sipa_schemas.PengajuanCreate, db: Session = Depe
 def get_all_pengajuan(db: Session = Depends(get_db)):
     return db.query(sipa_models.PengajuanJadwal).all()
 
+@router.get("/{pengajuan_id}", response_model=sipa_schemas.PengajuanResponse)
+def get_pengajuan_by_id(pengajuan_id: int, db: Session = Depends(get_db)):
+    db_pengajuan = db.query(sipa_models.PengajuanJadwal).filter(sipa_models.PengajuanJadwal.id == pengajuan_id).first()
+    
+    if not db_pengajuan:
+        raise HTTPException(status_code=404, detail="Pengajuan tidak ditemukan")
+    
+    return db_pengajuan
+
 @router.patch("/{pengajuan_id}/status", response_model=sipa_schemas.PengajuanResponse)
 def update_status_pengajuan(
     pengajuan_id: int, 
